e5359ec157fa88271061f10033c44a9c7e6f3fef
FivePixels <o5pxels@gmail.com>
2019-01-28 22:34:31 -0600

    Assignments Completed
    I did them!

---
+++ Schoolwork/ChangeCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChangeCounter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int change = 92;
+            int quarter = 25;
+            int dime = 10;
+            int nickel = 5;
+            int penny = 1;
+            System.Console.WriteLine("         Changer App");
+            System.Console.Write("Change Amount: $0.{0}", change);
+            int reqQuarters = change / quarter;
+            int reqDime = (change % quarter) / dime;
+            int reqNickel = ((change % quarter) % dime) / nickel;
+            int reqPenny = (((change % quarter) % dime) % nickel) / penny;
+            System.Console.WriteLine("");
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Quarters:  {0}", reqQuarters);
+            System.Console.WriteLine("Dimes:     {0}", reqDime);
+            System.Console.WriteLine("Nickels:   {0}", reqNickel);
+            System.Console.WriteLine("Pennies:   {0}", reqPenny);
+        }
+    }
+}
---
+++ Schoolwork/GradingApp.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using static System.Math;
+
+namespace GradingApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double total = 0;
+            int numberOfScores = 5;
+            double average;
+            int[] scoresArray = { 34, 3, 32, 18, 231 };
+            foreach (double val in scoresArray)
+                total += val;
+            average = Round(total / numberOfScores, 2);
+            WriteLine("  Grading App");
+            WriteLine("");
+            WriteLine("Score #1:  {0}", scoresArray[0]);
+            WriteLine("Score #2:  {0}", scoresArray[1]);
+            WriteLine("Score #3:  {0}", scoresArray[2]);
+            WriteLine("Score #4:  {0}", scoresArray[3]);
+            WriteLine("Score #5:  {0}", scoresArray[4]);
+            WriteLine("");
+            WriteLine("");
+            WriteLine("Average:   {0:F}", average);
+            }
+        }
+        }
